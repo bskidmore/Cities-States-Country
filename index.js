381d5c69418dfe033c228a76c3bd641dba5b729d
Swapnil <swapnil.nakhate1010@gmail.com>
2019-10-15 23:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,41 +6,37 @@ var alasql = require("alasql");
 module.exports = {
 
 	getAllCountries: function() {
-        return countries;
-    },
+        	return countries;
+    	},
 
 	getAllStates: function() {
-        return states;
-    },
+        	return states;
+    	},
 
 	getAllStatesFromCountry: function(countryName) {
-        let stateCountryJoinQuery = 'SELECT state.* FROM ? as state WHERE state.country_id=(VALUE OF SELECT * FROM ? AS country WHERE country.name="'+countryName+'")';
-        let cityStateJoinList = alasql(cityStateJoinQuery, [cityList, states]);
-        return cityStateJoinList;
-    },
+		let stateCountryJoinQuery = 'SELECT state.* FROM ? as state WHERE state.country_id=(VALUE OF SELECT * FROM ? AS country WHERE country.name="'+countryName+'")';
+		let stateCountryJoinList = alasql(stateCountryJoinQuery, [cityList, states]);
+		return stateCountryJoinList;
+    	},
 
-    getAllCitiesFromState: function(stateName) {
-        return cities;
-    },
+	searchCity: function(searchTextCity) {
+		let searchCityQuery = 'SELECT * FROM ? as city WHERE name LIKE "'+searchTextCity+'%"';
+		let cityList = alasql(searchCityQuery, [cities]);
 
-    searchCity: function(searchTextCity) {
-        let searchCityQuery = 'SELECT * FROM ? as city WHERE name LIKE "'+searchTextCity+'%"';
-        let cityList = alasql(searchCityQuery, [cities]);
+		let cityStateJoinQuery = 'SELECT city.id as cityId, city.name as cityName, city.state_id, state.name as stateName, state.country_id FROM ? as city JOIN ? as state ON city.state_id=state.id';
+		let cityStateJoinList = alasql(cityStateJoinQuery, [cityList, states]);
 
-        let cityStateJoinQuery = 'SELECT city.id as cityId, city.name as cityName, city.state_id, state.name as stateName, state.country_id FROM ? as city JOIN ? as state ON city.state_id=state.id';
-        let cityStateJoinList = alasql(cityStateJoinQuery, [cityList, states]);
+		let cityStateCountryJoinQuery = 'SELECT state.*, country.name as countryName, country.sortname as countryShortName, country.phoneCode as phoneCode FROM ? as state JOIN ? as country ON state.country_id=country.id';
+		let cityStateCountryJoinList = alasql(cityStateCountryJoinQuery, [cityStateJoinList, countries]);
+		return cityStateCountryJoinList;
+	},
 
-        let cityStateCountryJoinQuery = 'SELECT state.*, country.name as countryName, country.sortname as countryShortName, country.phoneCode as phoneCode FROM ? as state JOIN ? as country ON state.country_id=country.id';
-        let cityStateCountryJoinList = alasql(cityStateCountryJoinQuery, [cityStateJoinList, countries]);
-        return cityStateCountryJoinList;
-    },
+	searchState: function(searchTextState) {
+		let searchStateQuery = 'SELECT * FROM ? as state WHERE name LIKE "'+searchTextState+'%"';
+		let statesList = alasql(searchStateQuery, [states]);
 
-    searchState: function(searchTextState) {
-        let searchStateQuery = 'SELECT * FROM ? as state WHERE name LIKE "'+searchTextState+'%"';
-        let statesList = alasql(searchStateQuery, [states]);
-
-        let stateCountryJoinQuery = 'SELECT state.*, country.name as countryName, country.sortname as countryShortName, country.phoneCode as phoneCode FROM ? as state JOIN ? as country ON state.country_id=country.id';
-        let stateCountryList = alasql(stateCountryJoinQuery, [statesList, countries]);
-        return stateCountryList;
-    }
+		let stateCountryJoinQuery = 'SELECT state.*, country.name as countryName, country.sortname as countryShortName, country.phoneCode as phoneCode FROM ? as state JOIN ? as country ON state.country_id=country.id';
+		let stateCountryList = alasql(stateCountryJoinQuery, [statesList, countries]);
+		return stateCountryList;
+	}
 }
